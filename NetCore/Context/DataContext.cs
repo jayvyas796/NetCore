b30e3cadb6c8faf8eadc18c9c4e844b22ce34f5e
jayvyas796 <31070930+jayvyas796@users.noreply.github.com>
2020-12-29 19:27:15 +0530

    commit all

---
+++ NetCore/Context/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore.Context
+{
+
+    public class DataContext : DbContext
+    {
+        public DataContext() { }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+
+}
